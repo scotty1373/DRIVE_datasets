16cd8607045e3e14455daddbc81977e4d1746457
Data-Mark 4 <1024935841@qq.com>
2021-06-15 20:07:16 +0800

    update and complete code logic

--- datasets.py
+++ datasets.py
@@ -21,13 +21,14 @@ else:
 
 def origin_img_split(path):
     data_ = np.array(Image.open(path))
-    gray_data_ = cv2.cvtColor(data_, cv2.COLOR_BGR2GRAY)
-    padding_data_ = np.zeros((588, 567))
-    padding_data_[2:586, 1:566] = gray_data_
+    # gray_data_ = cv2.cvtColor(data_, cv2.COLOR_BGR2GRAY)
+    gray_data_ = data_[:, :, 1]
+    padding_data_ = np.zeros((590, 571))
+    padding_data_[3:587, 3:568] = gray_data_
     img_patch = np.empty((1, 584*565, 7, 7))
     count = 0
-    for i in range(581):
-        for j in range(560):
+    for i in range(584):
+        for j in range(565):
             img_patch[0, count, :, :] = padding_data_[i: i + 7, j: j + 7]
             # print(img_patch[0, count, :, :].shape, padding_data_[i * 7:i * 7 + 7, j * 7:j * 7 + 7].shape)
             count += 1
@@ -36,13 +37,14 @@ def origin_img_split(path):
 
 def label_img_split(path):
     data_ = np.array(Image.open(path))
-    padding_data_ = np.ones((588, 567))
-    padding_data_[2:586, 1:566] = data_
-    img_patch = np.empty((1, 584*565, 7, 7))
+    img_patch = np.empty((1, 584*565))
     count = 0
-    for i in range(581):
-        for j in range(560):
-            img_patch[0, count, :, :] = padding_data_[i: i + 7, j: j + 7]
+    for i in range(584):
+        for j in range(565):
+            if data_[i, j] == 255.:
+                img_patch[0, count] = 1
+            else:
+                img_patch[0, count] = 0
             # print(img_patch[0, count, :, :].shape, padding_data_[i * 7:i * 7 + 7, j * 7:j * 7 + 7].shape)
             count += 1
     return img_patch
@@ -52,25 +54,32 @@ class FCN:
     def __init__(self):
         self.in_shape = (7, 7, 1)
         self.lr = 0.001
-        self.out_shape = 49
+        self.out_shape = 2
         self.batchsize = 64
-        self.epochs = 5
+        self.epochs = 3
         self.net = self.net_builder()
 
     def net_builder(self):
         input_ = keras.Input(self.in_shape, dtype='float', name='patch input')
-        common = keras.layers.Conv2D(3, (3, 3), strides=(1, 1), activation='sigmoid')(input_)
-        common = keras.layers.Conv2D(7, (3, 3), strides=(1, 1), activation='sigmoid')(common)
+        common = keras.layers.Conv2D(3, (3, 3), strides=(1, 1), activation='relu')(input_)
+        common = keras.layers.Conv2D(9, (3, 3), strides=(1, 1), activation='relu')(common)
+        common = keras.layers.Conv2D(13, (3, 3), strides=(1, 1), padding='same', activation='relu')(common)
+        common = keras.layers.Conv2D(17, (3, 3), strides=(1, 1), padding='same', activation='relu')(common)
+        common = keras.layers.Conv2D(21, (3, 3), strides=(1, 1), padding='same', activation='relu')(common)
+        common = keras.layers.BatchNormalization()(common)
         common = keras.layers.Flatten()(common)
-        common = keras.layers.Dense(units=128, activation='sigmoid')(common)
-        common = keras.layers.Dense(units=self.out_shape, activation='sigmoid')(common)
+        common = keras.layers.Dense(units=64, activation='relu')(common)
+        common = keras.layers.Dense(units=self.out_shape, activation='softmax')(common)
 
         model = keras.Model(inputs=input_, outputs=common)
         # plot_model(model, to_file='net_builder.png')
         return model
 
-    def train_iter(self, train_x, train_y):
+    def compulate_loss(self):
         pass
+
+    def train_one_batch(self, train_x, train_y):
+        logits = self.net.fit(train_y, train_x)
 
 
 if __name__ == '__main__':
@@ -80,7 +89,7 @@ if __name__ == '__main__':
 
     path_dir_lab = os.path.join(path_dir, 'DRIVE', 'test', '1st_manual').replace('\\', '/')
     file_list_lab = os.listdir(path_dir_lab)
-    img_label = np.empty((len(file_list_lab[:offset]), patch_size, 7, 7))
+    img_label = np.empty((len(file_list_lab[:offset]), patch_size))
 
     counter = 0
     for file in file_list_img[:offset]:
@@ -89,20 +98,32 @@ if __name__ == '__main__':
         img_origin[counter, :, :, :] = img_iter
         counter += 1
         time.time()
+
     counter = 0
     for file in file_list_lab[:offset]:
         os.chdir(path_dir_lab)
         img_iter = label_img_split(file)
-        img_label[counter, :, :, :] = img_iter
+        img_label[counter, :] = img_iter.reshape(-1,)
         counter += 1
         time.time()
-    # db_train = tf.data.Dataset.from_tensor_slices((np.split(img_origin, img_origin.shape[1], axis=1),
-    #                                               np.split(img_label, img_label.shape[1], axis=1)))
+
+    img_origin = (img_origin/255).squeeze().reshape((-1, 7, 7, 1))
+    img_label[img_label == 255.] = 1
+
+    img_label = tf.one_hot(img_label.squeeze(), depth=2)
+    db_train = tf.data.Dataset.from_tensor_slices((img_origin, img_label)).shuffle(500).batch(10)
     drive = FCN()
-    drive.net.compile(loss=keras.losses.binary_crossentropy, metrics=['accuracy', 'loss'])
-    for epoch in range(drive.epochs):
+    drive.net.compile(loss=keras.losses.mse, optimizer=keras.optimizers.Adam(learning_rate=0.001), metrics=['accuracy'])
+    drive.net.fit(db_train, epochs=drive.epochs)
+
+    pred_img = drive.net.predict(np.array(img_origin).reshape((-1, 7, 7, 1)))
+    pred_data = np.array(tf.argmax(pred_img, axis=1)).reshape((584, 565))
+    plt.imshow(pred_data, cmap='gray')
+    plt.show()
 
 
-
-
-    time.time()
+    # for epoch in range(drive.epochs):
+    #     for index, (train_x, train_y) in enumerate(db_train):
+    #         drive.net.train_on_batch(train_x, train_y)
+    #         evalute_data = drive.net.evaluate(train_x, train_y, verbose=1)
+    #         print(f'acc: {evalute_data[1]}, loss: {evalute_data[0]}')
