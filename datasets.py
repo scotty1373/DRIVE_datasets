62dd9cca57485e6c2a86e1619a5a2cef337948d1
scotty1373 <spsim1373@outlook.com>
2021-06-24 14:28:23 +0800

    os.listdir mothod update, complete linux and windwos, update train on branch mode

--- datasets.py
+++ datasets.py
@@ -56,19 +56,19 @@ class FCN:
         self.lr = 0.001
         self.out_shape = 2
         self.batchsize = 64
-        self.epochs = 4
+        self.epochs = 5
         self.net = self.net_builder()
 
     def net_builder(self):
         input_ = keras.Input(self.in_shape, dtype='float', name='patch input')
         common = keras.layers.Conv2D(3, (3, 3), strides=(1, 1), activation='relu')(input_)
         common = keras.layers.Conv2D(7, (3, 3), strides=(1, 1), activation='relu')(common)
-        common = keras.layers.Conv2D(13, (3, 3), strides=(1, 1), padding='same', activation='relu')(common)
-        common = keras.layers.Conv2D(17, (3, 3), strides=(1, 1), padding='same', activation='relu')(common)
+        common = keras.layers.Conv2D(15, (3, 3), strides=(1, 1), padding='same', activation='relu')(common)
         common = keras.layers.Conv2D(21, (3, 3), strides=(1, 1), padding='same', activation='relu')(common)
+        common = keras.layers.Conv2D(27, (3, 3), strides=(1, 1), padding='same',  activation='relu')(common)
         common = keras.layers.BatchNormalization()(common)
         common = keras.layers.Flatten()(common)
-        common = keras.layers.Dense(units=64, activation='relu')(common)
+        common = keras.layers.Dense(units=64, activation='sigmoid')(common)
         common = keras.layers.Dense(units=self.out_shape, activation='softmax')(common)
 
         model = keras.Model(inputs=input_, outputs=common)
@@ -85,15 +85,18 @@ class FCN:
 if __name__ == '__main__':
     path_dir_img = os.path.join(path_dir, 'DRIVE', 'test', 'images').replace('\\', '/')
     file_list_img = os.listdir(path_dir_img)
+    file_list_img.sort()
     img_origin = np.empty((len(file_list_img[:offset]), patch_size, 7, 7))
 
     path_dir_lab = os.path.join(path_dir, 'DRIVE', 'test', '1st_manual').replace('\\', '/')
     file_list_lab = os.listdir(path_dir_lab)
+    file_list_lab.sort()
     img_label = np.empty((len(file_list_lab[:offset]), patch_size))
 
     counter = 0
     for file in file_list_img[:offset]:
         os.chdir(path_dir_img)
+        print(f'image file name: {file}')
         img_iter = origin_img_split(file)
         img_origin[counter, :, :, :] = img_iter
         counter += 1
@@ -102,28 +105,33 @@ if __name__ == '__main__':
     counter = 0
     for file in file_list_lab[:offset]:
         os.chdir(path_dir_lab)
+        print(f'label file name: {file}')
         img_iter = label_img_split(file)
         img_label[counter, :] = img_iter.reshape(-1,)
         counter += 1
         time.time()
 
     img_origin = (img_origin/255).squeeze().reshape((-1, 7, 7, 1))
-    img_label[img_label == 255.] = 1
 
     img_label = tf.one_hot(img_label.squeeze(), depth=2)
-    db_train = tf.data.Dataset.from_tensor_slices((img_origin, img_label)).shuffle(1000).batch(15)
+    db_train = tf.data.Dataset.from_tensor_slices((img_origin, img_label)).shuffle(500).batch(20)
     drive = FCN()
     drive.net.compile(loss=keras.losses.mse, optimizer=keras.optimizers.Adam(learning_rate=0.001), metrics=['accuracy'])
-    drive.net.fit(db_train, epochs=drive.epochs)
 
+    # Train on batch
+    # for epoch in range(drive.epochs):
+    #     loss_history = np.array([])
+    #     acc_history = np.array([])
+    #     print(f'epoch {epoch}/{drive.epochs}')
+    #     for index, (train_x, train_y) in enumerate(db_train):
+    #         loss, acc = drive.net.train_on_batch(train_x, train_y)
+    #         loss_history = np.append(loss_history, loss)
+    #         acc_history = np.append(acc_history, acc)
+    #     print(f'current rpoch:{epoch}, loss: {loss_history.mean()}, accurency:{acc_history.mean()}')
+
+    # Teain on datasets
+    drive.net.fit(db_train, epochs=drive.epochs)
     pred_img = drive.net.predict(np.array(img_origin).reshape((-1, 7, 7, 1)))
     pred_data = np.array(tf.argmax(pred_img, axis=1)).reshape((584, 565))
     plt.imshow(pred_data, cmap='gray')
     plt.show()
-
-
-    # for epoch in range(drive.epochs):
-    #     for index, (train_x, train_y) in enumerate(db_train):
-    #         drive.net.train_on_batch(train_x, train_y)
-    #         evalute_data = drive.net.evaluate(train_x, train_y, verbose=1)
-    #         print(f'acc: {evalute_data[1]}, loss: {evalute_data[0]}')
